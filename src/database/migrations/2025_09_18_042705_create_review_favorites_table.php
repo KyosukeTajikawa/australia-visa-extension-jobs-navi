90708cf17de29f13d95e8d72f1e411bdb6888b7b
github bajiou <5114232.u@gmail.com>
2025-09-18 17:44:07 +0900

    マイグレーションファイル・テーブル作成

--- src/database/migrations/2025_09_18_042705_create_review_favorites_table.php
+++ src/database/migrations/2025_09_18_042705_create_review_favorites_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->timestamp('created_at')->useCurrent();
             // 複合主キー
             $table->primary(['user_id', 'review_id']);
-        });
     }
 
     /**

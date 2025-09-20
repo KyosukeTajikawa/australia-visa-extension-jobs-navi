fc620487548b2b4088e256123c3054d020f6b78c
github bajiou <5114232.u@gmail.com>
2025-09-20 12:55:15 +0900

    vendorとsanctumのアンインストール

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit19b21fff8d5a641822e8695dec1c544a::getLoader();
+return ComposerAutoloaderInitd751713988987e9331980363e24189ce::getLoader();

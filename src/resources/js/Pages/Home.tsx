9f254a6fab661f91c682bc7d1260a404f6f4cdd3
github bajiou <5114232.u@gmail.com>
2025-09-22 19:35:33 +0900

    home.tsxの余分ページ削除

--- src/resources/js/Pages/Home.tsx
+++ src/resources/js/Pages/Home.tsx
@@ -21,44 +21,6 @@ const Home = ({ farms }) => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     return (
         <Box m={2}>
-            {/* ヘッダー */}
-            {/* <Box bg={"green.500"} p={"4px"} mb={2} display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
-                <Heading as={"h1"} color={"white"} fontSize={{ base: "24px", md: "30px", lg: "40px" }}>ファーム一覧</Heading> */}
-                {/* SPメニュー */}
-                {/* <Box display={{ base: "block", md: "none" }}>
-                    <Menu>
-                        <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon color={"white"} />} variant={"ghost"} _hover={{ bg: "green.300" }} _active={{ bg: "green.300" }} />
-                        <MenuList>
-                            <MenuItem>ログイン</MenuItem>
-                            <MenuItem>新規登録</MenuItem>
-                        </MenuList>
-                    </Menu>
-                </Box> */}
-                {/* PCメニュー */}
-                {/* <Box display={{ base: "none", md: "block" }}>
-                    <Button colorScheme='teal' onClick={onOpen} bg={"green.500"} _hover={{ bg: "green.300" }}>
-                        <HamburgerIcon />
-                    </Button>
-                    <Drawer isOpen={isOpen} onClose={onClose}>
-                        <DrawerOverlay />
-                        <DrawerContent>
-                            <DrawerCloseButton />
-                            <DrawerBody mt={"60px"} align={"center"} w={"100%"}>
-                                <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
-                                    <Link _hover={{ color: "none" }}>
-                                        <Text>ログイン</Text>
-                                    </Link>
-                                </Box>
-                                <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
-                                    <Link _hover={{ color: "none" }}>
-                                        <Text>新規登録</Text>
-                                    </Link>
-                                </Box>
-                            </DrawerBody>
-                        </DrawerContent>
-                    </Drawer>
-                </Box>
-            </Box> */}
             {/* ファーム一覧 */}
             <VStack spacing={4} align={"stretch"}>
                 {farms.map((farm) => (
@@ -75,12 +37,6 @@ const Home = ({ farms }) => {
                     </Link>
                 ))}
             </VStack>
-            {/* Footer */}
-            {/* <Box>
-                <Box bg="green.500" color={"white"} fontWeight={"bold"} textAlign={"center"} py={{ base: 2, md: 3 }}>
-                    <Text fontSize={{ base: 13, md: 16 }}>&copy; 2025 ファーム攻略サイト</Text>
-                </Box>
-            </Box > */}
         </Box >
     );
 };

76bb2b465631a0482c12ad5a3dbe4047d295f8ac
github bajiou <5114232.u@gmail.com>
2025-09-22 22:57:13 +0900

    未ログイン・ログイン時のヘッダー切り替え作成

--- src/resources/js/Layouts/MainLayout.tsx
+++ src/resources/js/Layouts/MainLayout.tsx
@@ -9,48 +9,94 @@ import { Link as InertiaLink, usePage } from "@inertiajs/react"
 type AuthUser = { id: number; name: string } | null;
 
 const MainLayout = ({ children, title }) => {
-    const { auth } = usePage().props as { auth: { user: AuthUser } };
+    const { auth } = usePage().props;
     const { isOpen, onOpen, onClose } = useDisclosure()
     return (
         <Box m={2}>
             {/* ヘッダー */}
-            {auth.user ? <Text>{auth.user.name}</Text> : <Text>ログインなし</Text>}
             <Box bg={"green.500"} p={"4px"} mb={2} display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
                 <Heading as={"h1"} color={"white"} fontSize={{ base: "24px", md: "30px", lg: "40px" }}>ファーム一覧</Heading>
-                {/* SPメニュー */}
-                <Box display={{ base: "block", md: "none" }}>
-                    <Menu>
-                        <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon color={"white"} />} variant={"ghost"} _hover={{ bg: "green.300" }} _active={{ bg: "green.300" }} />
-                        <MenuList>
-                            <MenuItem>ログイン</MenuItem>
-                            <MenuItem>新規登録</MenuItem>
-                        </MenuList>
-                    </Menu>
-                </Box>
-                {/* PCメニュー */}
-                <Box display={{ base: "none", md: "block" }}>
-                    <Button colorScheme='teal' onClick={onOpen} bg={"green.500"} _hover={{ bg: "green.300" }}>
-                        <HamburgerIcon />
-                    </Button>
-                    <Drawer isOpen={isOpen} onClose={onClose}>
-                        <DrawerOverlay />
-                        <DrawerContent>
-                            <DrawerCloseButton />
-                            <DrawerBody mt={"60px"} align={"center"} w={"100%"}>
-                                <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
-                                    <Link _hover={{ color: "none" }}>
-                                        <Text>ログイン</Text>
-                                    </Link>
-                                </Box>
-                                <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
-                                    <Link _hover={{ color: "none" }}>
-                                        <Text>新規登録</Text>
-                                    </Link>
-                                </Box>
-                            </DrawerBody>
-                        </DrawerContent>
-                    </Drawer>
-                </Box>
+                {auth.user ?
+                    /* SP ログイン*/
+                    < Box display={{ base: "block", md: "none" }}>
+                        <Menu>
+                            <MenuButton as={IconButton} rightIcon={<ChevronDownIcon color={"white"} />} color={"white"} variant={"ghost"} _hover={{ bg: "green.300" }} _active={{ bg: "green.300" }}>
+                                {auth.user.nickname}
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>ファーム登録</MenuItem>
+                                <MenuItem>お気に入りレビュー</MenuItem>
+                                <MenuItem>ログアウト</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Box>
+                    /* SP 未ログイン*/
+                    : <Box display={{ base: "block", md: "none" }}>
+                        <Menu>
+                            <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon color={"white"} />} variant={"ghost"} _hover={{ bg: "green.300" }} _active={{ bg: "green.300" }} />
+                            <MenuList>
+                                <MenuItem>ログイン</MenuItem>
+                                <MenuItem>新規登録</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Box>
+                }
+                {auth.user ?
+                    /* PC ログイン*/
+                    <Box display={{ base: "none", md: "block" }}>
+                        <Button colorScheme='teal' onClick={onOpen} bg={"green.500"} _hover={{ bg: "green.300" }}>
+                            <HamburgerIcon />
+                        </Button>
+                        <Drawer isOpen={isOpen} onClose={onClose}>
+                            <DrawerOverlay />
+                            <DrawerContent>
+                                <DrawerCloseButton />
+                                <DrawerHeader>{auth.user.nickname}</DrawerHeader>
+                                <DrawerBody mt={"60px"} align={"center"} w={"100%"}>
+                                    <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
+                                        <Link _hover={{ color: "none" }}>
+                                            <Text>ファーム登録</Text>
+                                        </Link>
+                                    </Box>
+                                    <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
+                                        <Link _hover={{ color: "none" }}>
+                                            <Text>お気に入りレビュー</Text>
+                                        </Link>
+                                    </Box>
+                                    <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
+                                        <Link _hover={{ color: "none" }}>
+                                            <Text>ログアウト</Text>
+                                        </Link>
+                                    </Box>
+                                </DrawerBody>
+                            </DrawerContent>
+                        </Drawer>
+                    </Box>
+                    /* PC 未ログイン*/
+                    : <Box display={{ base: "none", md: "block" }}>
+                        <Button colorScheme='teal' onClick={onOpen} bg={"green.500"} _hover={{ bg: "green.300" }}>
+                            <HamburgerIcon />
+                        </Button>
+                        <Drawer isOpen={isOpen} onClose={onClose}>
+                            <DrawerOverlay />
+                            <DrawerContent>
+                                <DrawerCloseButton />
+                                <DrawerBody mt={"60px"} align={"center"} w={"100%"}>
+                                    <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
+                                        <Link _hover={{ color: "none" }}>
+                                            <Text>ログイン</Text>
+                                        </Link>
+                                    </Box>
+                                    <Box mb={1} _hover={{ opacity: 0.7, bg: "gray.100" }}>
+                                        <Link _hover={{ color: "none" }}>
+                                            <Text>新規登録</Text>
+                                        </Link>
+                                    </Box>
+                                </DrawerBody>
+                            </DrawerContent>
+                        </Drawer>
+                    </Box>
+                }
             </Box>
             <Box>{children}</Box>
             {/* Footer */}
@@ -59,7 +105,7 @@ const MainLayout = ({ children, title }) => {
                     <Text fontSize={{ base: 13, md: 16 }}>&copy; 2025 ファーム攻略サイト</Text>
                 </Box>
             </Box >
-        </Box>
+        </Box >
     );
 };
 

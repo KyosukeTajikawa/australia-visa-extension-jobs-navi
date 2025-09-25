75e4a865a9bbffb164c485aee36175454ea2ab00
github bajiou <5114232.u@gmail.com>
2025-09-25 14:21:23 +0900

    インポートの記載方法

--- src/app/Http/Controllers/FarmController.php
+++ src/app/Http/Controllers/FarmController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Farm;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class FarmController extends Controller
 {
     /**
      * 農場の一覧ページを表示
      *@return \Inertia\Response
-    */
-    public function index(): \Inertia\Response
+     */
+    public function index(): Response
     {
 
         $farms = Farm::get();

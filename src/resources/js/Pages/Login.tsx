c367aebf8b0cacf5964c3a05bd8364bf65d68096
github bajiou <5114232.u@gmail.com>
2025-09-29 08:38:59 +0900

    breezeファイル削除

--- src/resources/js/Pages/Login.tsx
+++ src/resources/js/Pages/Login.tsx
@@ -6,7 +6,7 @@ import MainLayout from "@/Layouts/MainLayout";
 
 const Login = () => {
 
-    const { data, setData, post, processing, errors} = useForm({
+    const { data, setData, post, processing, errors } = useForm({
         email: "",
         password: "",
     });
@@ -18,20 +18,20 @@ const Login = () => {
 
 
     return (
-        <Box w={{base:"90%", md:"60%"}} mx={"auto"} mt={"150px"}>
+        <Box w={{ base: "90%", md: "60%" }} mx={"auto"} mt={"150px"}>
             <Heading as={"h3"} mb={4} fontWeight={"bold"} fontSize={{ base: "18px", md: "24px" }} color={"gray.600"}><LockIcon />ログイン</Heading>
             <form onSubmit={handleSubmit}>
                 <FormControl mb={2} isRequired isInvalid={!!errors.email}>
-                <FormLabel htmlFor="email">メールアドレス</FormLabel>
-                    <Input id="email" name="email" type="email" placeholder="例)test@example.com" value={data.email} onChange={(e) => setData("email", e.target.value)}/>
+                    <FormLabel htmlFor="email">メールアドレス</FormLabel>
+                    <Input id="email" name="email" type="email" placeholder="例)test@example.com" value={data.email} onChange={(e) => setData("email", e.target.value)} />
                     <FormErrorMessage>{errors.email}</FormErrorMessage>
-            </FormControl>
+                </FormControl>
                 <FormControl mb={4} isRequired isInvalid={!!errors.password}>
-                <FormLabel htmlFor="password">パスワード</FormLabel>
-                    <Input id="password" name="password" placeholder="●●●●●●" type="password" value={data.password} onChange={(e) => setData("password", e.target.value)}/>
+                    <FormLabel htmlFor="password">パスワード</FormLabel>
+                    <Input id="password" name="password" placeholder="●●●●●●" type="password" value={data.password} onChange={(e) => setData("password", e.target.value)} />
                     <FormErrorMessage>{errors.email}</FormErrorMessage>
-            </FormControl>
-            <Button type="submit" colorScheme={"green"} isLoading={processing}>ログイン</Button>
+                </FormControl>
+                <Button type="submit" colorScheme={"green"} isLoading={processing}>ログイン</Button>
             </form>
         </Box>
     )

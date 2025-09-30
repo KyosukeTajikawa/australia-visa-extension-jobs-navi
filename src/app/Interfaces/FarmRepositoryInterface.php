8381290fcbb5b956c4858e5d2225701c54a5628d
github bajiou <5114232.u@gmail.com>
2025-09-30 11:38:27 +0900

    PHPdoc追加

--- src/app/Interfaces/FarmRepositoryInterface.php
+++ src/app/Interfaces/FarmRepositoryInterface.php
@@ -4,6 +4,17 @@ namespace App\Interfaces;
 
 interface FarmRepositoryInterface
 {
+    /**
+     * すべてのファーム情報を取得する
+     * @return Collection<Farm>
+     */
     public function getAllFarms();
+
+    /**
+     * 指定したIDのファーム詳細を取得する
+     * レビュー情報（reviews）と州情報（state）も同時に取得する。
+     * @param int $id 取得したいファームのID
+     * @return Farm|null 該当するFarmモデル。存在しない場合はnull
+     */
     public function getDetailById(int $id);
 }

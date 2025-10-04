76f26c14878712f509405042e96785abdc082527
github bajiou <5114232.u@gmail.com>
2025-10-04 17:36:10 +0900

    NOTE追加

--- src/database/factories/FarmFactory.php
+++ src/database/factories/FarmFactory.php
@@ -24,6 +24,9 @@ class FarmFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'street_address' => fake()->streetAddress(),
             'suburb' => fake()->streetName(),
+            // NOTE: 本番の時に使用
+            // 'state_id' => fake()->numberBetween(1, 8),
+            // NOTE: テストの時はこちらを使用
             'state_id' => State::factory(),
             'postcode' => fake()->postcode2(),
             'phone_number' => fake()->unique()->phoneNumber(),

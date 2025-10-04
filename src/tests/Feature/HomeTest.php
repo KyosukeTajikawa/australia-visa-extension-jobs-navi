9be38dc0daf9cbcdc250b56df5c37d9d528ea645
github bajiou <5114232.u@gmail.com>
2025-10-04 13:59:40 +0900

    HomeTest完了

---
+++ src/tests/Feature/HomeTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Farm;
+use App\Models\State;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_home_receive_farm_props(): void
+    {
+        $user = User::factory()->create();
+        $state = State::factory()->create();
+
+        Farm::factory()
+            ->count(2)
+            ->for($user, 'user')
+            ->for($state, 'state')
+            ->create();
+
+        $response = $this->get('/home');
+        //画面が開くか
+        $response->assertStatus(200);
+        //viewにDBからのデータが渡っているか。中身が正しいか。
+        $response->assertInertia(
+            fn(Assert $page) => $page
+                ->component('Home')
+                ->has('farms', 2)
+                ->has(
+                    'farms.0',
+                    fn(Assert $farm) => $farm
+                        ->hasAll(['id', 'name'])
+                        ->etc()
+                )
+        );
+    }
+}
--- src/tests/Unit/FarmRepositoryTest.php
+++ src/tests/Unit/FarmRepositoryTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Unit\Repositories;
 
 use App\Models\Farm;
+use App\Models\Review;
 use App\Models\State;
 use App\Models\User;
 use App\Repositories\FarmRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -15,16 +17,13 @@ class FarmRepositoryTest extends TestCase
 
     private FarmRepository $repo;
 
-    /**
-     * A basic unit test example.
-     */
     protected function setUp(): void
     {
         parent::setUp();
         $this->repo = new FarmRepository();
     }
 
-    public function test_get_all_farms()
+    public function test_get_all_farms(): void
     {
 
         $state = State::factory()->create();
@@ -39,5 +38,38 @@ class FarmRepositoryTest extends TestCase
 
         $this->assertCount(2, $farms);
         $this->assertInstanceOf(Farm::class, $farms->first());
+    }
+
+    public function test_detail_by_id_not_come_with_reviews_and_state(): void
+    {
+        $farm = Farm::factory()->create();
+        $found = $this->repo->getDetailById($farm->id);
+
+        $this->assertSame($farm->id, $found->id);
+        $this->assertFalse($found->relationLoaded('reviews'));
+        $this->assertFalse($found->relationLoaded('state'));
+    }
+
+    public function test_detail_by_id_come_with_reviews_and_state(): void
+    {
+        $state = State::factory()->create();
+
+        $farm = Farm::factory()
+        ->for($state, 'state')
+        ->has(Review::factory()->count(2), 'reviews')
+        ->create();
+
+        $found = $this->repo->getDetailById($farm->id, ['state', 'reviews']);
+
+        $this->assertTrue($found->relationLoaded('reviews'));
+        $this->assertCount(2, $found->reviews);
+        $this->assertTrue($found->relationLoaded('state'));
+        $this->assertSame($state->id, $found->state->id);
+    }
+
+    public function test_get_detail_by_id_throws_when_not_found(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+        $this->repo->getDetailById(999999);
     }
 }

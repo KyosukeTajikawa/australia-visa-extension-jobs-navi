7bd3f66007e3fd0d75a5771ee4297dced5cf47b8
github bajiou <5114232.u@gmail.com>
2025-10-05 15:05:23 +0900

    FarmFactoryの不要なコードを削除

--- src/database/factories/FarmFactory.php
+++ src/database/factories/FarmFactory.php
@@ -24,14 +24,8 @@ class FarmFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'street_address' => fake()->streetAddress(),
             'suburb' => fake()->streetName(),
-            // NOTE: 本番の時に使用
-            // 'state_id' => fake()->numberBetween(1, 8),
-            // NOTE: テストの時はこちらを使用
             'state_id' => State::factory(),
             'postcode' => fake()->postcode2(),
-            'postcode' => fake()->postcode2(),
-            'phone_number' => fake()->unique()->phoneNumber(),
-            'email' => fake()->unique()->safeEmail(),
             'description' => fake()->realText(200),
             'created_user_id' => User::factory(),
         ];

771238e1a2387cbe8fa06a532c686174998e1177
github bajiou <5114232.u@gmail.com>
2025-10-25 11:21:31 +0900

    FavoritesStoreTest作成

--- src/tests/Feature/ReviewController/FavoritesStoreTest.php
+++ src/tests/Feature/ReviewController/FavoritesStoreTest.php
@@ -2,19 +2,39 @@
 
 namespace Tests\Feature\ReviewController;
 
+use App\Models\Farm;
+use App\Models\Review;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
 
 class FavoritesStoreTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * favoritesStoreの処理の確認テスト
+     * ルート：作成したルートをたどるか
+     * pivot：review_favoritesテーブルに保存されるか
+     */
+    public function testFavoritesStore(): void
+    {
+        $user = User::factory()->create();
+        $farm = Farm::factory()->create();
+        $review = Review::factory()->create();
+
+        $response = $this->actingAs($user)->post(route('favorites.store', ['review' => $review->id]));
+
+        $response->assertStatus(302);
+
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('review_favorites', [
+            'review_id' => $review->id,
+            'user_id' => $user->id,
+        ]);
+
+        $response->assertRedirect(route('review.favorites'));
     }
 }

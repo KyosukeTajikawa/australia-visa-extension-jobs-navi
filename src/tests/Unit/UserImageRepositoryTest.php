5b104e89955ef3cc6364ba6688babda2a889941b
github bajiou <5114232.u@gmail.com>
2025-11-11 12:12:14 +0900

    UserImageRepositoryTestに追加

---
+++ src/tests/Unit/UserImageRepositoryTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Models\UserImage;
+use App\Repositories\Auth\UserImageRepositoryInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserImageRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private UserImageRepositoryInterface $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = app(UserImageRepositoryInterface::class);
+    }
+
+    /**
+     * registerImage()のテスト
+     * registerImage() 登録できている
+     */
+    public function testRegisterImage(): void
+    {
+        $user = User::factory()->create();
+
+        $UserImage = [
+            'user_id' => $user->id,
+            'url' => 'user/1/sample1.jpg',
+            'path' => 'sample1.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        $this->repository->registerImage($UserImage);
+
+        $this->assertDatabaseHas('user_images', [
+            'user_id' => $user->id,
+            'url' => $UserImage['url'],
+            'path' => $UserImage['path'],
+        ]);
+    }
+
+    /**
+     * getImage()のテスト
+     * getImage() 取得できている
+     */
+    public function testGetImage(): void
+    {
+        $user = User::factory()->create();
+
+        $UserImage = [
+            'user_id' => $user->id,
+            'url' => 'user/1/sample1.jpg',
+            'path' => 'sample1.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        $this->repository->registerImage($UserImage);
+        $result = $this->repository->getImage($user);
+
+        $this->assertSame($UserImage['user_id'], $result->user_id);
+        $this->assertSame($UserImage['url'], $result->url);
+        $this->assertSame($UserImage['path'], $result->path);
+    }
+
+    /**
+     * updateImage()のテスト
+     * updateImage() 更新できている
+     */
+    public function testUpdateImage(): void
+    {
+        $user = User::factory()->create();
+
+        $userImage = [
+            'user_id' => $user->id,
+            'url' => 'user/1/sample1.jpg',
+            'path' => 'sample1.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        $previousImage = UserImage::create($userImage);
+
+        $newImage = [
+            'user_id' => $user->id,
+            'url' => 'user/2/sample1.jpg',
+            'path' => 'sample2.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        $this->repository->updateImage($newImage, $previousImage);
+
+        $this->assertDatabaseHas('user_images', [
+            'user_id' => $user->id,
+            'url' => 'user/2/sample1.jpg',
+            'path' => 'sample2.jpg',
+        ]);
+    }
+
+}

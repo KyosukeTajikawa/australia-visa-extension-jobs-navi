e7520d172eca8af3696e71ebb4052387a27bd9f6
github bajiou <5114232.u@gmail.com>
2025-11-11 13:02:01 +0900

    IndexTest作成

---
+++ src/tests/Feature/RegisteredUserController/IndexTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\RegisteredUserController;
+
+use App\Models\Crop;
+use App\Models\State;
+use App\Models\User;
+use App\Models\UserImage;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * Createの確認
+     * 州・作物情報がCreateに渡っているか確認
+     */
+    public function testCreateWithStatesAndCrops(): void
+    {
+        $user = User::factory()->create();
+
+        $ImageData = [
+            'user_id' => $user->id,
+            'url' => 'user/1/sample1.jpg',
+            'path' => 'sample1.jpg',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        $userImage = UserImage::create($ImageData);
+
+        $response = $this->actingAs($user)->get('/profile');
+
+        $response->assertInertia(
+            fn(Assert $page) => $page
+                ->component('Auth/Profile')
+                ->where('user.id', $user->id)
+                ->where('user.nickname', $user->nickname)
+                ->where('user_image.user_id', $user->id)
+                ->where('user_image.url', $userImage->url)
+                ->where('user_image.path', $userImage->path)
+        );
+    }
+
+    /**
+     * 未ログイン者をloginにredirectするか
+     */
+    public function testGuestTryAccessDetailButFail(): void
+    {
+        $response = $this->get('/profile');
+
+        $response->assertStatus(302);
+        $response->assertRedirect(route('login'));
+    }
+}

f627dfa31260571d357efc8d4db4c21d90d190ce
github bajiou <5114232.u@gmail.com>
2025-11-18 15:39:17 +0900

    AdminDashboardControllerTestの作成

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'nickname' => fake()->name(),
+            'nickname' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'gender' => fake()->numberBetween(1, 2),

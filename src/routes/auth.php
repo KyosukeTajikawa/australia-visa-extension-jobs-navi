bad1d9cbfeccdd2a2fa7b821ceddcf3e9a83f1e0
github bajiou <5114232.u@gmail.com>
2025-12-04 22:05:48 +0900

    不要なファイル削除

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -42,24 +42,6 @@ Route::middleware('auth')->group(function () {
 
     Route::delete('/{id}/destroy', [RegisteredUserController::class, 'imageDestroy'])->name('user.image.destroy');
 
-    Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
-
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-
     Route::delete('destroy', [RegisteredUserController::class, 'destroy'])
         ->name('user.destroy');
 
